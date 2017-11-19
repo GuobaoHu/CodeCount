c633173cc921dcd601300edc064d7af4a8eb515e
HuGuobao <398456458@qq.com>
2017-11-19 13:21:21 +0800

    新建CodeStastic项目，添加主Class: CodeCount

---
+++ src/guyue/hu/CodeCounts.java
@@ -0,0 +1,14 @@
+package guyue.hu;
+/**
+*@author guyue E-mail:398456458@qq.com
+*version1.0 创建时间:2017年11月19日 下午1:19:52
+*describ 
+*/
+public class CodeCounts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
